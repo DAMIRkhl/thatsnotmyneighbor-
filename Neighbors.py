6b0ca86ba65fd7692e116a5954d0819a9520f73c
Damir <dkhalimov2012@gmail.com>
2024-06-21 16:58:38 -0400

    ctuyrg

---
+++ Neighbors.py
@@ -0,0 +1,9 @@
+import arcade
+
+
+class People (arcade.Sprite):
+    def __init__(self,id,phone_number,floor,looks,apartment_number):
+        super().__init__()
+class MonstersThatProtendToBePeople (arcade.Sprite):
+    def __init__(self):
+        super().__init__()
--- ThatsNotMyNeighbor.py
+++ ThatsNotMyNeighbor.py
@@ -5,11 +5,31 @@ import arcade
 
 class FirstGame(arcade.Window):
     def __init__(self, width, height):
-        super().__init__(width, height)
+        super().__init__(width, height,fullscreen=True)
         self.BG = arcade.load_texture("Damir (2).png")
+        self.officeBg = arcade.load_texture("Office_Background.webp")
+        hurt_sound = arcade.load_sound("a6f6-a2d8-4389-a606-173ef0cd7ef2.wav")
+        arcade.play_sound(hurt_sound)
+        self.offset_x = 0
+        self.offset_y = 0
+        self.max_offset_x = 300
+        self.max_offset_y = 200
+
+        self.some_sprite = arcade.Sprite('buttons2.png', 2)
+        self.some_sprite.center_x = self.width / 2
+        self.some_sprite.center_y = self.height / 2
+
     def on_draw(self):
         self.clear()
-        arcade.draw_texture_rectangle(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2 , SCREEN_WIDTH, SCREEN_HEIGHT, self.BG)
+        arcade.draw_texture_rectangle(self.width / 2 + self.offset_x,
+                                      self.height / 2 + self.offset_y,
+                                      self.width + 600, self.height + 400, self.officeBg)
+        arcade.draw_texture_rectangle(self.width / 2 + self.offset_x,
+                                      self.height / 2 + self.offset_y,
+                                      self.width + 600, self.height + 400, self.BG)
+
+        # self.some_sprite.draw()
+
     def update(self, delta_time: float):
         pass
 
@@ -17,10 +37,25 @@ class FirstGame(arcade.Window):
         pass
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
-        pass
+        self.offset_x -= dx
+        self.offset_y -= dy
+        print(dx,dy)
+
+        if self.offset_x > self.max_offset_x:
+            self.offset_x = self.max_offset_x
+        elif self.offset_x < -self.max_offset_x:
+            self.offset_x = -self.max_offset_x
+
+        if self.offset_y > self.max_offset_y:
+            self.offset_y = self.max_offset_y
+        elif self.offset_y < -self.max_offset_y:
+            self.offset_y = -self.max_offset_y
+
 
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
         pass
+
+
 
 
 window = FirstGame(SCREEN_WIDTH, SCREEN_HEIGHT)
